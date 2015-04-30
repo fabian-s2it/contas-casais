e56a43b37eb03e72a3cee98eb5d0088a1eb03017
linkzao <fabiancabau@hotmail.com>
2015-04-29 21:48:47 -0300

    Added transaction model and changed args parser

--- app.py
+++ app.py
@@ -6,17 +6,12 @@ from flask.ext.httpauth import HTTPBasicAuth
 from models import *
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root@localhost/contas'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root@127.0.0.1/contas'
 
 auth = HTTPBasicAuth()
 
 db = SQLAlchemy(app)
 api = restful.Api(app)
-
-parser = reqparse.RequestParser()
-parser.add_argument('username', type=str)
-parser.add_argument('email', type=str)
-parser.add_argument('password', type=str)
 
 
 @auth.verify_password
@@ -28,6 +23,13 @@ def verify_password(username, password):
 
 
 class Users(Resource):
+
+    def __init__(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('username', type=str)
+        parser.add_argument('email', type=str)
+        parser.add_argument('password', type=str)
+
     @auth.login_required
     def get(self, user_id):
         user = User.query.get(user_id)
@@ -35,8 +37,15 @@ class Users(Resource):
 
 
 class UserList(Resource):
+
+    def __init__(self):
+        self.parser = reqparse.RequestParser()
+        self.parser.add_argument('username', type=str)
+        self.parser.add_argument('email', type=str)
+        self.parser.add_argument('password', type=str)
+
     def post(self):
-        args = parser.parse_args()
+        args = self.parser.parse_args()
         user = User(args['username'], args['email'], args['password'])
 
         db.session.add(user)
@@ -44,8 +53,32 @@ class UserList(Resource):
 
         return True, 200
 
+
+class Transactions(Resource):
+
+    def __init__(self):
+        self.parser = reqparse.RequestParser()
+        self.parser.add_argument('user_id', type=int)
+        self.parser.add_argument('category', type=int)
+        self.parser.add_argument('type', type=int)
+        self.parser.add_argument('amount', type=float)
+        self.parser.add_argument('description', type=str)
+
+
+
+    @auth.login_required
+    def post(self):
+        args = self.parser.parse_args()
+        transaction = Transaction(args['user_id'], args['category'], args['type'],
+                                  args['amount'], args['description']
+                                 )
+
+        db.session.add(transaction)
+        db.session.commit()
+
 api.add_resource(Users, '/users/<user_id>')
 api.add_resource(UserList, '/user/')
+api.add_resource(Transactions, '/transaction/')
 
 if __name__ == '__main__':
     app.run(debug=True, port=2323)
--- models.py
+++ models.py
@@ -1,12 +1,14 @@
 from hashlib import md5
 from app import db
+from datetime import datetime
 
 class Couple(db.Model):
 
     __tablename__ = 'couple'
 
-    user_id_c1 = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
-    user_id_c2 = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
+    user_id_c1 = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user_id_c2 = db.Column(db.Integer, db.ForeignKey('user.id'))
 
     def __init__(self, c1, c2):
         self.user_id_c1 = c1
@@ -19,6 +21,7 @@ class User(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True)
+    transactions = db.relationship('Transaction', backref='transaction', lazy='dynamic')
     email = db.Column(db.String(120), unique=True)
     password = db.Column(db.String(120), unique=False)
 
@@ -60,64 +63,68 @@ class Transaction(db.Model):
     __tablename__ = 'transaction'
 
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True)
-    email = db.Column(db.String(120), unique=True)
-    password = db.Column(db.String(120), unique=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    category = db.Column(db.Integer)
+    type = db.Column(db.Integer)
+    amount = db.Column(db.Float)
+    description = db.Column(db.String(120), unique=False)
+    datetime = db.Column(db.DateTime, default=datetime.now())
 
-    def __init__(self, username, email, password):
-        self.username = username
-        self.email = email
-        self.password = password
+    def __init__(self, user_id, category, type, amount, description):
+        self.user_id = user_id
+        self.category = category
+        self.type = type
+        self.amount = amount
+        self.description = description
 
     def __repr__(self):
-        return '<User %r>' % self.username
-
-    def passwordify(self):
-        self.password = (md5(self.email.encode('utf-8')).hexdigest(), 18)
-
-    def create_couple(self, couple_id):
-        couple = Couple(self.id, couple_id)
-        db.session.add(couple)
-        db.session.commit()
+        return '<Description %r>' % self.description
 
     @property
     def serialize(self):
        return {
            'id'         : self.id,
-           'username': self.username,
-           'email': self.email,
-           'password': self.password
+           'user_id': self.user_id,
+           'category': self.category,
+           'type': self.type,
+           'amount': self.amount,
+           'description': self.description,
+           'datetime': self.datetime.strftime('%m/%d/%Y')
        }
 
 class CoupleTransaction(db.Model):
 
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True)
-    email = db.Column(db.String(120), unique=True)
-    password = db.Column(db.String(120), unique=False)
+    __tablename__ = 'couple_transaction'
 
-    def __init__(self, username, email, password):
-        self.username = username
-        self.email = email
-        self.password = password
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    couple_id = db.Column(db.Integer, db.ForeignKey('couple.id'))
+    category = db.Column(db.Integer)
+    type = db.Column(db.Integer)
+    amount = db.Column(db.Float)
+    description = db.Column(db.String(120), unique=False)
+    datetime = db.Column(db.DateTime, default=datetime.now())
+
+    def __init__(self, user_id, couple_id, category, type, amount, description):
+        self.user_id = user_id
+        self.couple_id = couple_id
+        self.category = category
+        self.type = type
+        self.amount = amount
+        self.description = description
 
     def __repr__(self):
-        return '<User %r>' % self.username
-
-    def passwordify(self):
-        self.password = (md5(self.email.encode('utf-8')).hexdigest(), 18)
-
-    def create_couple(self, couple_id):
-        couple = Couple(self.id, couple_id)
-        db.session.add(couple)
-        db.session.commit()
+        return '<Description %r>' % self.description
 
     @property
     def serialize(self):
        return {
            'id'         : self.id,
-           'username': self.username,
-           'email': self.email,
-           'password': self.password
+           'user_id': self.user_id,
+           'couple_id': self.couple_id,
+           'category': self.category,
+           'type': self.type,
+           'amount': self.amount,
+           'description': self.description,
+           'datetime': self.datetime.strftime('%m/%d/%Y')
        }
-
